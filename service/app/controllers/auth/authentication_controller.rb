dc1ab9c62ba7776d1c92edb7715551208a14718c
UTakuto <taku.0307.ue@icloud.com>
2025-07-28 15:56:03 +0900

    add: 認証機能のregisterおよびloginメソッドを追加

--- service/app/controllers/auth/authentication_controller.rb
+++ service/app/controllers/auth/authentication_controller.rb
@@ -1,2 +1,37 @@
 class Auth::AuthenticationController < ApplicationController
+  # POST auth/register
+  def register
+    user = User.new(user_params)
+    if user.save
+      render json: {
+        name: user.name,
+        email: user.email
+        # ,
+        # role: user.role,
+        # class_name: user.class_name,
+      }, status: :created
+    else
+      render json: {
+        error: user.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
+  
+  # POST auth/login
+  def login
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      token = JsonWebToken.encode(user_id: user.id)
+      render json: { token: token }, status: :ok
+    else
+      render json: { error: 'メールアドレスまたはパスワードが違います' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation, :role, :class_name)
+  end
+
 end
