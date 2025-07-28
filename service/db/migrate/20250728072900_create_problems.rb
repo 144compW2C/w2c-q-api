bec8c24df474c115d22e39298b4fd04c4fd9396e
UTakuto <taku.0307.ue@icloud.com>
2025-07-28 16:43:49 +0900

    add: problemsテーブルを作成し、必要なカラムを追加

---
+++ service/db/migrate/20250728072900_create_problems.rb
@@ -0,0 +1,19 @@
+class CreateProblems < ActiveRecord::Migration[7.1]
+  def change
+    create_table :problems, id: :bigint do |t|
+      t.text :title , null: false
+      t.text :body
+      t.references :tag, foreign_key: true
+      t.references :status, foreign_key: true
+      t.references :creator, null: false, foreign_key: { to_table: :users }
+      t.references :reviewer, foreign_key: { to_table: :users }
+      t.integer :level
+      t.integer :difficulty
+      t.boolean :is_multiple_choice
+      t.datetime :reviewed_at
+      t.boolean :delete_flag, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
