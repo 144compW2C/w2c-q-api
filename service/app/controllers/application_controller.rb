492e5f186de942662babe93269d4230b8e71e108
UTakuto <taku.0307.ue@icloud.com>
2025-07-28 15:23:52 +0900

    add: 認証リクエストを処理するメソッドを追加

--- service/app/controllers/application_controller.rb
+++ service/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  def authenticate_request!
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header.present?
+    decoded = JsonWebToken.decode(header)
+
+    if decoded && User.exists?(id: decoded[:user_id])
+      @current_user = User.find(decoded[:user_id])
+    else
+      render json: { error: '認証エラー' }, status: :unauthorized
+    end
+  end
 end
