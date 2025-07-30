e249dd3ac8903c6075ae84d0cf2e5c5d2c3d2a24
UTakuto <taku.0307.ue@icloud.com>
2025-07-30 10:34:42 +0900

    update: routes.rbにユーザーリソースのCRUD操作を追加

--- service/config/routes.rb
+++ service/config/routes.rb
@@ -4,5 +4,28 @@ Rails.application.routes.draw do
     post 'register', to: 'authentication#register'
   end
 
-  resources :users, only: [:index, :show, :create, :update, :destroy] 
+  # 一般利用者向けAPI
+  resources :problems, only: [:index, :show] do
+    get 'modelAnswers', to: 'problems#model_answer', on: :member
+    resources :options, only: [:index, :create]
+  end
+
+  namespace :storage do
+    resources :answers, only: [:index, :show, :create, :update]
+    resources :problems, only: [:create, :update]
+  end
+
+  # 管理者用API
+  namespace :admin do
+    resources :problems, only: [:index, :show, :destroy] do
+      post 'approve', on: :member
+      put 'organize', on: :member
+    end
+  end
+
+  # その他管理リソース
+  resources :tags, only: [:index, :show]
+  resources :statuses, only: [:index, :show]
+  resources :users, only: [:index, :show, :update]
+
 end
